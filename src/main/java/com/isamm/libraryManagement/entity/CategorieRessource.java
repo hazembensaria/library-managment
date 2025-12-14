2c1c41aaea11fe7eae4fcb7fbd980250e84ecaf5
rahmabenomrane <rahmabenomrane5@gmail.com>
2025-12-15 00:15:53 +0100

    CRUD des  ressources + upload/prévisualisation de documents + interface UI

---
+++ src/main/java/com/isamm/libraryManagement/entity/CategorieRessource.java
@@ -0,0 +1,24 @@
+package com.isamm.libraryManagement.entity;
+
+public enum CategorieRessource {
+    NOVEL,
+    DETECTIVE,
+    SCIENCE_FICTION,
+    FANTASY,
+    HISTORICAL,
+    BIOGRAPHY,
+    POETRY,
+    THEATER,
+    COOKING,
+    TRAVEL,
+    SPORTS,
+    MUSIC,
+    CINEMA,
+    ART,
+    PHILOSOPHY,
+    PSYCHOLOGY,
+    ECONOMICS,
+    INFORMATICS,
+    LANGUAGE
+
+}
